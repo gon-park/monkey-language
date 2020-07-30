1008205b2a7e59130f5537b7c00a01cdc16f7136
gon-88 <gon.88@kakaocorp.com>
2020-07-30 09:26:19 +0900

    Add repl

--- token/token.go
+++ token/token.go
@@ -11,21 +11,44 @@ const (
 	EOF       = "EOF"
 	IDENT     = "IDENT" // add, foobar, x, y, ...
 	INT       = "INT"
-	ASSIGN    = "="
-	PLUS      = "+"
 	COMMA     = ","
 	SEMICOLON = ";"
 	LPAREN    = "("
 	RPAREN    = ")"
 	LBRACE    = "{"
 	RBRACE    = "}"
-	FUNCTION  = "FUNCTION"
-	LET       = "LET"
+
+	EQ     = "=="
+	NOT_EQ = "!="
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	IF       = "IF"
+	ELSE     = "ELSE"
+	RETURN   = "RETURN"
+
+	// Operators
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	ASTERISK = "*"
+	SLASH    = "/"
+	LT       = "<"
+	GT       = ">"
 )
 
 var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
+	"fn":     FUNCTION,
+	"let":    LET,
+	"true":   TRUE,
+	"false":  FALSE,
+	"if":     IF,
+	"else":   ELSE,
+	"return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {
