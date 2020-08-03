d661f06a38ed711db84a0151c9df72ee5047d823
gon-88 <gon.88@kakaocorp.com>
2020-08-03 10:01:52 +0900

    Add page 100

--- repl/repl.go
+++ repl/repl.go
@@ -6,13 +6,28 @@ import (
 	"io"
 
 	"github.daumkakao.com/gon-park/monkey/lexer"
-	"github.daumkakao.com/gon-park/monkey/token"
+	"github.daumkakao.com/gon-park/monkey/parser"
 )
 
 const PROMPT = ">> "
 
+const MONKEY_FACE = `
+           __,__
+  .--.  .-"     "-.  .--.
+ / .. \/  .-. .-.  \/ .. \
+| |  '|  /   Y   \  |'  | | 
+| \   \  \ 0 | 0 /  /   / |
+ \ '- ,\.-"""""""-./, -' /
+  ''-' /_   ^ ^   _\ '-''
+      |  \._   _./  |
+      \   \ '~' /   /
+       '._ '-=-' _.'
+          '-----'
+`
+
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -21,10 +36,26 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		line := scanner.Text()
-
 		l := lexer.New(line)
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		p := parser.New(l)
+
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	io.WriteString(out, MONKEY_FACE)
+	io.WriteString(out, "Woops! We ran into some monkey business here!\n")
+	io.WriteString(out, " parser errors:\n")
+
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
